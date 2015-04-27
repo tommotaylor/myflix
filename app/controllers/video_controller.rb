4414800d6efcb3a76304e2cb387cfd26770ca697
Tom Taylor <tommotaylor@gmail.com>
2015-04-27 13:57:50 +0100

    create show video page

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,6 +1,18 @@
 class VideoController < ApplicationController
 
   def index
+  	@videos = Video.all
   end
+
+  def show
+  	@video = Video.first
+  end
+
+private
+
+  def video_params
+  	params.require(:video).permit(:title, :description, :small_cover_url, :large_cover_url)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get 'home', to: 'video#index'
+  get 'video', to: 'video#show'
+
 end
