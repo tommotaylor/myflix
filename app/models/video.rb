b4b536220b3fa6872868ec95eaf899ade87ffaa8
tommotaylor <tommotaylor@gmail.com>
2015-05-18 14:37:01 +0100

    create search_by_title on video model with tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,9 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    results = Video.where("title LIKE ?", "%#{search_term}%")
+  end
+
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,5 +6,25 @@ describe Video do
   it { should validate_presence_of (:title) }
   it { should validate_presence_of (:description) }
 
+  describe "#search_by_title" do
+    it "returns an empty array when no result" do
+      result = Video.search_by_title("")
+      result.should eq([])
+    end
+
+    it "returns an array with one title when there is one match" do
+      Video.create(title: "black fish", description: "awesome")
+      result = Video.search_by_title("black fish")
+      result.length.should eq(1)
+    end
+
+    it "returns an array with more than one title when there is more than one match" do
+      Video.create(title: "black fish", description: "awesome")
+      Video.create(title: "black fish", description: "really awesome")
+      result = Video.search_by_title("black fish")
+      result.length.should eq(2)
+    end
+  end
+  
 
 end
