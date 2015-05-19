912bc8a5157d2d8062d7abc92588d226a8929664
tommotaylor <tommotaylor@gmail.com>
2015-05-19 10:45:30 +0100

    fix issue with proxy collections in specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
   has_many :videos
+
+  def recent_videos
+    videos
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,28 @@ require "rails_helper"
 describe Category do
   it { should have_many (:videos)}
 
+describe "#recent_videos" do
+ 
+  it "should return only videos from the named category" do
+    sci_fi = Category.create(name: "science fiction")
+    documentary = Category.create(name: "documentary")
+    interstellar = Video.create(title: "interstellar", description: "awesome", category: sci_fi)
+    supermensch = Video.create(title: "supermensch", description: "really awesome", category: documentary)
+    expect(documentary.recent_videos).to include(supermensch)
+  end
+
+  it "should return all videos if less than 6 exist" do
+    documentary = Category.create(name: "documentary")
+    black_fish = Video.create(title: "black fish", description: "awesome", category: documentary, created_at: 1.day.ago)
+    supermensch = Video.create(title: "supermensch", description: "really awesome", category: documentary)
+    expect(documentary.recent_videos).to eq([black_fish, supermensch])
+  end
+
+  it "should return 6 videos in descending order if more than 6 exist" do
+  end
+
+
+end
+
+
 end
