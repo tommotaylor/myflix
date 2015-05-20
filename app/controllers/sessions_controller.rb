8dcffbe941fa133bd80c9977643cda808244b7dc
tommotaylor <tommotaylor@gmail.com>
2015-05-20 16:56:08 +0100

    create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,23 @@
 class SessionsController < ApplicationController
-
   def new
-  end
-  
 
+  end
+
+  def create
+    user = User.where(email: params[:email]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, you've logged in."
+      redirect_to video_path
+    else
+      flash[:error] = "There is something wrong with your username or password."
+      redirect_to sign_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You've logged out."
+    redirect_to front_path
+  end
 end
