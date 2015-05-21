b4d8dc78a1e2219db4513100ff4f9c8654fbb17a
tommotaylor <tommotaylor@gmail.com>
2015-05-21 15:45:01 +0100

    add user validations, can't get haml form to save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,31 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      #session[:user_id] = @user.id
+      flash[:notice] = "Thanks for registering"
+      redirect_to video_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :email, :name
 end
