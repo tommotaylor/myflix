d6fdb2e079a3e392c9b3c875ef4a2c92ca4efca3
tommotaylor <tommotaylor@gmail.com>
2015-05-21 16:55:46 +0100

    fix register form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.save
       #session[:user_id] = @user.id
       flash[:notice] = "Thanks for registering"
-      redirect_to video_path
+      redirect_to home_path
     else
       render :new
     end
