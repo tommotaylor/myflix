9d0d275edadcbb26773c0de2b2101658628d93b3
tommotaylor <tommotaylor@gmail.com>
2015-05-23 14:06:26 +0100

    refactor VideosController spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,40 +1,30 @@
 require 'rails_helper'
 
 describe VideosController do
-
   describe "GET show"  do
-    context "with authenticated user" do
-      before do
-        session[:user_id] = Fabricate(:user)
-      end
-      it "assigns the requested video to @video" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(assigns(:video)).to eq(video)
-      end
-      it "renders the show template" do
-        video = Fabricate(:video)
-        get :show, id: video
-        expect(response).to render_template(:show)
-      end
+    it "assigns the requested video to @video when signed in" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
+    it "redirects user to login if not signed in" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to(:sign_in)
     end
   end
-
   describe "GET search" do
-    context "with authenticated user" do
-      before do
-        session[:user_id] = Fabricate(:user)
-      end
-      it "assigns @results" do
-        video = Fabricate(:video)
-        get :search, search_term: "#{video.title}"
-        expect(assigns(:results)).to include(video)
-      end
-      it "renders the search template" do
-        video = Fabricate(:video)
-        get :search, search_term: "#{video.title}"
-        expect(response).to render_template(:search)
-      end
+    it "assigns @results when signed in" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :search, search_term: "#{video.title}"
+      expect(assigns(:results)).to eq([video])
+    end
+    it "redirects user to login if not signed in" do
+      video = Fabricate(:video)
+      get :search, search_term: "#{video.title}"
+      expect(response).to redirect_to(:sign_in)
     end
   end
 end
