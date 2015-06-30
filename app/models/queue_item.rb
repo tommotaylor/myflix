3ca14d70b14c4799743dda396ad941e31426a93f
tommotaylor <tommotaylor@gmail.com>
2015-06-30 12:22:21 +0200

    build class methods for QueueItem with tests

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -3,4 +3,17 @@ class QueueItem < ActiveRecord::Base
   belongs_to :video
   validates_presence_of :video_id, :user_id
   validates_uniqueness_of :video_id
+
+  delegate :category, to: :video
+  delegate :title, to: :video, prefix: :video
+
+  def category_name
+    category.name
+  end
+
+  def rating
+    review = Review.where(video_id: video.id, user_id: user.id).first
+    review.rating if review
+  end
+
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,4 +3,50 @@ require 'rails_helper'
 describe QueueItem do
   it { should belong_to (:video)}
   it { should belong_to (:user)}
+
+  describe "#category_name" do
+    it "returns category name of queue item" do
+      category = Fabricate(:category, name: "drama")
+      video = Fabricate(:video, category_id: category.id)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video_id: video.id, user_id: user.id)
+      expect(queue_item.category_name).to eq(category.name)
+    end
+  end
+
+  describe "#category" do
+    it "returns the category of the queue item" do
+      category = Fabricate(:category, name: "drama")
+      video = Fabricate(:video, category_id: category.id)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video_id: video.id, user_id: user.id)
+      expect(queue_item.category).to eq(category)
+    end
+  end
+
+  describe "#video_title" do
+    it "returns the title of the queue items video" do
+      category = Fabricate(:category, name: "drama")
+      video = Fabricate(:video, category_id: category.id, title: "Dig")
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video_id: video.id, user_id: user.id)
+      expect(queue_item.video_title).to eq(video.title)
+    end
+  end
+
+  describe "#rating" do
+    it "returns the users rating of the video" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, user_id: user.id, video_id: video.id, rating: 4)
+      queue_item = Fabricate(:queue_item, video_id: video.id, user_id: user.id)
+      expect(queue_item.rating).to eq(4)
+    end
+    it "returns nil if there is no rating" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video_id: video.id, user_id: user.id)
+      expect(queue_item.rating).to eq(nil)
+    end
+  end
 end
