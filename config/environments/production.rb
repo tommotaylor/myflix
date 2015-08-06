2bffbc122ac7cff889618a67b722c8a805241698
tommotaylor <tommotaylor@gmail.com>
2015-08-06 10:12:37 +0100

    configuring mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,8 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
-    user_name:            '<username>',
-    password:             '<password>',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
