52e5b2992cbf5a6344632223abc49212dd2e7929
tommotaylor <tommotaylor@gmail.com>
2015-08-20 14:41:58 +0100

    working on minimagick image processing

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -10,7 +10,8 @@ before_action :require_admin
   def create
     @video = Video.new(video_params)
     if @video.save
-      redirect_to @video 
+      resize_images(@video)
+      redirect_to @video
     else
       flash[:error] = "Please complete all fields"
       render :new
@@ -21,5 +22,12 @@ private
 
   def video_params
     params.require(:video).permit(:title, :description, :small_cover, :large_cover, :category_id)
+  end
+
+  def resize_images(video)
+    small_cover = MiniMagick::Image.new(video.small_cover_url)
+    small_cover.resize "166X236"
+    large_cover = MiniMagick::Image.new(video.large_cover_url)
+    large_cover.resize "665X375"
   end
 end
