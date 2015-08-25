e6bca2d80b572dfc1f97ef53913f1be8e1abf484
tommotaylor <tommotaylor@gmail.com>
2015-08-25 11:05:21 +0100

    struggling

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-    config.aws_acl    = :'public-read'
+    config.aws_acl    = 'public-read'
 
     config.aws_credentials = {
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
