ff3f2e07d47a4c7859e2dc1e53d377b0f812afa6
tommotaylor <tommotaylor@gmail.com>
2015-08-25 14:49:58 +0100

    changes to uploaders for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,63 +12,66 @@ Category.create!([ {name: 'drama'},
 User.create!([
   { name: "Tom Taylor",
     email: "tom@tom.com",
-    password: "password"
+    password: "password",
+    admin: true
     },
   { name: "Lucie Watson",
     email: "lucie@lucie.com",
-    password: "password"
+    password: "password",
+    admin: false
     },
   { name: "Bob Dylan",
     email: "bob@bob.com",
-    password: "password" }])
+    password: "password",
+    admin: false }])
 
 Video.create!([
   { title: 'Birdman', 
     description: 'A washed-up actor, who once played an iconic superhero, battles his ego and attempts to recover his family, his career and himself in the days leading up to the opening of his Broadway play.',
-    small_cover_url: 'tmp/birdman.jpg',
-    large_cover_url: 'tmp/large1.jpg',
+    small_cover: 'tmp/birdman.jpg',
+    large_cover: 'tmp/large1.jpg',
     category_id: 1 
     },
   { title: 'Family Guy', 
     description: 'The ultimate desert car chase',
-    small_cover_url: 'tmp/family_guy.jpg',
-    large_cover_url: 'tmp/large2.jpg',
+    small_cover: 'tmp/family_guy.jpg',
+    large_cover: 'tmp/large2.jpg',
     category_id: 2 
     },
   { title: 'Chinatown', 
     description: 'A classic about all things that make mans life valuable',
-    small_cover_url: 'tmp/chinatown.jpg',
-    large_cover_url: 'tmp/large3.jpg',
+    small_cover: 'tmp/chinatown.jpg',
+    large_cover: 'tmp/large3.jpg',
     category_id: 1 
     },
   { title: 'Eddie Murphy: Delirious', 
     description: 'Stand up classic from Eddie Murphy',
-    small_cover_url: 'tmp/eddie_murphy.jpg',
-    large_cover_url: 'tmp/large4.jpg',
+    small_cover: 'tmp/eddie_murphy.jpg',
+    large_cover: 'tmp/large4.jpg',
     category_id: 2 
     },
   { title: 'Platoon', 
     description: 'A Vietnam War Epic',
-    small_cover_url: 'tmp/platoon.jpg',
-    large_cover_url: 'tmp/large1.jpg',
+    small_cover: 'tmp/platoon.jpg',
+    large_cover: 'tmp/large1.jpg',
     category_id: 3 
     },
   { title: 'Loruz', 
     description: 'Cult Indie Foreign Nonsense',
-    small_cover_url: 'tmp/loruz.jpg',
-    large_cover_url: 'tmp/large2.jpg',
+    small_cover: 'tmp/loruz.jpg',
+    large_cover: 'tmp/large2.jpg',
     category_id: 1 
     },
   { title: 'Frank', 
     description: 'The rise and demise of an indie band on their way to SXSW',
-    small_cover_url: 'tmp/frank.jpg',
-    large_cover_url: 'tmp/large3.jpg',
+    small_cover: 'tmp/frank.jpg',
+    large_cover: 'tmp/large3.jpg',
     category_id: 1 
     },
   { title: 'Futurama', 
     description: 'From the makers of The Simpsons',
-    small_cover_url: 'tmp/futurama.jpg',
-    large_cover_url: 'tmp/large4.jpg',
+    small_cover: 'tmp/futurama.jpg',
+    large_cover: 'tmp/large4.jpg',
     category_id: 2 }])
 
 QueueItem.create!([
