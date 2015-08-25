b261d69789d1614964b8755f3de6fb628e619825
tommotaylor <tommotaylor@gmail.com>
2015-08-25 07:39:26 +0100

    problems deploying

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? or Rails.env.production?
     config.storage    = :aws
-    config.aws_bucket = ENV.get('S3_BUCKET_NAME')
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = 'public-read'
 
     config.aws_credentials = {
-      access_key_id:     ENV.get('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.get('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.get('AWS_REGION')
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_REGION')
     }
   else
     config.storage = :file
