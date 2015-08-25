cd987c135b9d8c62de200a846d24e0d6dea12b69
tommotaylor <tommotaylor@gmail.com>
2015-08-25 07:43:55 +0100

    change notation of ENV variables in carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? or Rails.env.production?
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+    # config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = 'public-read'
 
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION')
+      :aws_bucket => ENV['S3_BUCKET_NAME']
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => ENV['AWS_REGION']
     }
   else
     config.storage = :file
