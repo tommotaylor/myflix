48237bc9573927515d687d4f38282c2cfd3e5fc8
tommotaylor <tommotaylor@gmail.com>
2015-08-25 07:54:43 +0100

    change uploader configs

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -8,13 +8,17 @@ class LargeCoverUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fill => [665, 375]
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def download_url(filename)
+    url(response_content_disposition: %Q{attachment; filename="#{filename}"})
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -7,7 +7,7 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
 
   process :resize_to_fill => [166, 236]
@@ -16,6 +16,10 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def download_url(filename)
+    url(response_content_disposition: %Q{attachment; filename="#{filename}"})
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
