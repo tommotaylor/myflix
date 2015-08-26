dca85129b42517c3a90d00c403c6a0e9d9b158e1
tommotaylor <tommotaylor@gmail.com>
2015-08-26 16:48:41 +0100

    add video url

---
+++ db/migrate/20150826153521_add_url_to_videos.rb
@@ -0,0 +1,5 @@
+class AddUrlToVideos < ActiveRecord::Migration
+  def change
+    add_column :videos, :video_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821082731) do
+ActiveRecord::Schema.define(version: 20150826153521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150821082731) do
     t.datetime "updated_at"
     t.string   "large_cover"
     t.string   "small_cover"
+    t.string   "video_url"
   end
 
 end
