4c2917d01ccf53875f5d897e3cf64056fb8b31d0
tommotaylor <tommotaylor@gmail.com>
2015-08-26 16:19:37 +0100

    deleted AWS region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-    # config.aws_acl    = 'public-read'
+    config.aws_acl    = 'public-read'
 
     config.aws_credentials = {
       access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION'),
       :path_style => true
     }
   else
