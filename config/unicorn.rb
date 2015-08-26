44fd1a3d00ad7c05ac7df54a466fd05a9417cf1d
tommotaylor <tommotaylor@gmail.com>
2015-08-26 16:01:47 +0100

    still trying

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,4 @@
+worker_processes 3
 timeout 15
 preload_app true
 
@@ -8,21 +9,19 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
+    config.redis = { size: 1 }
   end
   Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
+    config.redis = { size: 5 }
   end
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
