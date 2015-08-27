4bb33b7527063b91df889647da6082504ede1cc5
tommotaylor <tommotaylor@gmail.com>
2015-08-27 10:30:54 +0100

    finish admin_adds_video feature spec

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -21,7 +21,7 @@ before_action :require_admin
 private
 
   def video_params
-    params.require(:video).permit(:title, :description, :small_cover, :large_cover, :category_id)
+    params.require(:video).permit(:title, :description, :small_cover, :large_cover, :category_id, :video_url)
   end
 
 end
--- spec/support/feature_macros.rb
+++ spec/support/feature_macros.rb
@@ -8,6 +8,15 @@ module FeatureMacros
     click_button 'Sign In'
   end
 
+  def admin_sign_in(a_user=nil)
+    user = a_user || Fabricate(:user, admin: true)
+    visit sign_in_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+  end
+
+
   def sign_out
     visit sign_out_path
   end
