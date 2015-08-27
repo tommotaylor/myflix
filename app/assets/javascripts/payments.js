0b2b72e5721417ffce8fd2f23082ca441f16025c
tommotaylor <tommotaylor@gmail.com>
2015-08-27 18:52:56 +0100

    stripe errors

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -1,26 +1,22 @@
 jQuery(function($) {
   $('#payment-form').submit(function(event) {
     var $form = $(this);
-    $form.find('.payment_submit').prop('disabled', true);
-    Stripe.createToken({
-      number: $('.card-number').val(),
-      cvc: $('.card-cvc').val(),
-      exp_month: $('.card-expiry-month').val(),
-      exp_year: $('.card-expiry-year').val()  
-    }, stripeResponseHandler);
+    $form.find('button').prop('disabled', true);
+    Stripe.card.createToken($form, stripeResponseHandler);
+
     return false;
   });
-
-  var stripeResponseHandler = function(status, response) {
-    var $form = $('#payment-form');
-
-    if (response.error) {
-      $form.find('.payment-errors').text(response.error.mesage);
-      $form.find('.payment_submit').prop('disabled', false);
-    } else {
-      var token = response.id;
-      $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-      $form.get(0).submit();
-    }
-  };
 });
+
+function stripeResponseHandler(status, response) {
+  var $form = $('#payment-form');
+
+  if (response.error) {
+    $form.find('.payment-errors').text(response.error.message);
+    $form.find('button').prop('disabled', false);
+  } else {
+    var token = response.id;
+    $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+    $form.get(0).submit();
+  }
+};
