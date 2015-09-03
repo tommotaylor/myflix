e2b96cb578fa1ac551e0e197e9e8151b241e8e9c
tommotaylor <tommotaylor@gmail.com>
2015-09-03 09:49:54 +0100

    finish User controller create specs with Stripe implemented

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe UsersController do
 
   describe "GET new" do
+
     it "sets @user if not signed in" do
       get :new
       expect(assigns(:user)).to be_instance_of(User)
@@ -22,11 +23,12 @@ describe UsersController do
   describe "POST create" do
 
     context "with correct user and card info" do
+
       let(:charge) { double(:charge, successful?: true) }
       before do
-        StripeWrapper::Charge.stub(:create).and_return(charge)
+        expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
       end
-      
+
       it "creates a new user" do
         post :create, user: Fabricate.attributes_for(:user)
         expect(User.count).to eq(1)
@@ -37,7 +39,9 @@ describe UsersController do
       end
 
       context "sends emails" do
+
         after(:each) { ActionMailer::Base.deliveries.clear }
+        
         it "sends an email" do
           post :create, user: Fabricate.attributes_for(:user)        
           expect(ActionMailer::Base.deliveries).not_to be_empty
@@ -55,6 +59,7 @@ describe UsersController do
       end
 
       context "gets invited by a user" do
+
         it "creates a relationship where the invitor follows the friend" do
           invitor = Fabricate(:user)
           invite = Fabricate(:invite, user: invitor, token: 1234, friend_email: "friend@friend.com")
@@ -77,10 +82,14 @@ describe UsersController do
     end
 
     context "with invalid user info" do
+
       before do 
         post :create, user: Fabricate.attributes_for(:user, email: "")
       end
-      it "doesn't charge the card"
+
+      it "doesn't charge the card" do
+        expect(StripeWrapper::Charge).to_not receive(:create)
+      end
       it "doesn't create a new user" do
         expect(User.count).to eq(0)
       end
@@ -90,21 +99,21 @@ describe UsersController do
     end
 
     context "with valid user but declined card" do
+
+      let(:charge) { double(:charge, successful?: false, error_message: "Your card was declined") }
+      before do
+        expect(StripeWrapper::Charge).to receive(:create).and_return(charge)
+      end
+
       it "doesn't create a new user" do
-        charge = double(:charge, successful?: false)
-        StripeWrapper::Charge.stub(:create).and_return(charge)
         post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234567'
         expect(User.count).to eq(0)
       end
       it "renders the new template" do
-        charge = double(:charge, successful?: false)
-        StripeWrapper::Charge.stub(:create).and_return(charge)
         post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234567'
         expect(response).to render_template(:new)
       end
       it "sets the flash error message" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined")
-        StripeWrapper::Charge.stub(:create).and_return(charge)
         post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234567'
         expect(flash[:error]).to eq("Your card was declined")
       end
@@ -112,12 +121,12 @@ describe UsersController do
   end
 
   describe "GET show" do
+
     let(:user) {Fabricate(:user)}
 
     it_behaves_like "requires sign in" do
       let(:action) {get :show, id: user.id}
     end
-
     it "sets @user variable" do
       set_current_user
       get :show, id: user.id
